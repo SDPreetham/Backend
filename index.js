bc7a9f9acad1fe2d0f67d4d73bcc74e49cb3b1ca
SDPreetham <135338835+SDPreetham@users.noreply.github.com>
2023-08-16 07:35:18 +0000

    Ongoing

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+
+//Importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+//Database Collection
+const DbConnection = require("./dbConnection");
+dotenv.config();
+
 const app = express();
+DbConnection();
 const port = 8081;
 app.use(express.json());
 app.get("/",(req,res)=>{
@@ -8,6 +20,37 @@ message:"Server is up and running successfully",
     });
 });
 
+/**
+ * 
+ */
+app.use("/users",usersRouter);
+app.use("/books",booksRouter);
+
+
+
+
+
+
+/** 
+ * Route: /users/:id
+ * Method: GET
+ * Description: Get user by their ID
+ * Access:Public
+ * Parameters: id
+ */
+
+
+
+
+
+
+
+app.get("*",(req,res)=>{
+    res.status(404).json({
+        message:"This route does not exist",
+    });
+});
+
 app.listen(port,()=>{
     console.log(`Server is working at PORT ${port}`);
-})
+});
